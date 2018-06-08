3b47915575d405c87e4c306dd5a9d473ee05232a
Daniel Loth <daniel.loth@live.com>
2018-06-08 20:11:01 +1000

    Add a controller that works with MVC views.

---
+++ src/GCCS.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GCCS.Mvc.Controllers
+{
+    [Route("mvc")]
+    public class HomeController : Controller
+    {
+        [Route("hello")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
