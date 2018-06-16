13dd7309e3868741fad5138baca4231b6da9a01a
Daniel Loth <daniel.loth@live.com>
2018-06-16 18:15:16 +1000

    Add vendor.scss. Bootstrap now added via scss build.

--- src/GCCS.Mvc/gulpfile.js
+++ src/GCCS.Mvc/gulpfile.js
@@ -2,15 +2,31 @@
 
 var gulp = require("gulp"),
     fs = require("fs"),
+    autoprefixer = require("autoprefixer"),
+    postcss = require("gulp-postcss"),
+    sourceMaps = require("gulp-sourcemaps"),
     cssmin = require("gulp-cssmin"),
     rename = require("gulp-rename"),
     sass = require("gulp-sass");
 
-gulp.task("sass", function () {
+gulp.task("sass:site", function () {
     return gulp.src('Frontend/scss/site.scss')
         .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
         .pipe(gulp.dest('wwwroot/css'))
         .pipe(cssmin())
         .pipe(rename('site.min.css'))
         .pipe(gulp.dest('wwwroot/css'));
 });
+
+gulp.task("sass:vendor", function () {
+    return gulp.src('Frontend/scss/vendor.scss')
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(gulp.dest('wwwroot/css'))
+        .pipe(cssmin())
+        .pipe(rename('vendor.min.css'))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task("sass", ["sass:site", "sass:vendor"]);
