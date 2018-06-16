e4f972bfa3ff3595d361b306a2934b802fdc0d07
Daniel Loth <daniel.loth@live.com>
2018-06-16 19:18:21 +1000

    Add sourcemap generation.

--- src/GCCS.Mvc/gulpfile.js
+++ src/GCCS.Mvc/gulpfile.js
@@ -12,20 +12,28 @@ var gulp = require("gulp"),
 gulp.task("sass:site", function () {
     return gulp.src('Frontend/scss/site.scss')
         .pipe(sass())
+        .pipe(sourceMaps.init())
         .pipe(postcss([autoprefixer()]))
+        .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('wwwroot/css'))
         .pipe(cssmin())
         .pipe(rename('site.min.css'))
+        .pipe(sourceMaps.init())
+        .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('wwwroot/css'));
 });
 
 gulp.task("sass:vendor", function () {
     return gulp.src('Frontend/scss/vendor.scss')
         .pipe(sass())
+        .pipe(sourceMaps.init())
         .pipe(postcss([autoprefixer()]))
+        .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('wwwroot/css'))
         .pipe(cssmin())
         .pipe(rename('vendor.min.css'))
+        .pipe(sourceMaps.init())
+        .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('wwwroot/css'));
 });
 
